d5008e260d35ae07042b7124bf9ef49f0420355a
Sai Kumar <saikumar.kakarapalli@gmail.com>
2023-04-27 11:24:21 +0530

    dashboard styling & basic routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalcComponent } from './calc/calc.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+      {
+        path: "home",
+        component: DashboardComponent
+      },
+      {
+        "path": "Caluclator",
+        component: CalcComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/json/apps.ts
@@ -0,0 +1,20 @@
+const data = {
+    "appDetails": [
+        {
+            "name": "Caluclator",
+            "status": "Pending"
+        },
+        {
+            "name": "Crud",
+            "topics":"Cover All Inputs, all inputs with repeated inputs",
+            "status": "Pending"
+        },
+        {
+            "name": "Forms",
+            "topics":"Reactive (form builder / form group / form array), template driven",
+            "status": "Pending"
+        }
+    ]
+}
+
+export default data;
